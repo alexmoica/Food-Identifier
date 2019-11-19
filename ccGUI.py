e0fc47e0baa7d47b6ef398d0667494ddbe52a304
alex <alex@alexmoica.com>
2019-11-19 14:51:49 -0500

    Init commit

---
+++ ccGUI.py
@@ -0,0 +1,74 @@
+from Tkinter import *
+from PIL import Image, ImageTk
+import tkFileDialog
+import os
+import ccMain
+import ccDataHandler
+import cv2
+import atexit
+import numpy as np
+
+#define custom typesets
+LARGE_FONT = ('Verdana 15 underline')
+BOLD_FONT = ('Verdana 9 bold')
+global userFlag
+userFlag = 0
+
+global iconPA
+iconPA = []
+
+if __name__ == '__main__': #code only executed to run as a program not when simply imported as a module
+	root = Tk()
+	root.geometry('375x375')
+	root.wm_title("CarbonCamera")
+	root.resizable(width=False, height=False)
+
+	def exit_handler():
+		os.remove('userIcon.jpg') #remove icon image at exit
+
+	def open():
+		global userFlag #use global value of userFlag
+		global iconPA
+
+		#open file explorer for tkinter
+		img = tkFileDialog.askopenfilename(initialdir = os.path.dirname(os.path.abspath(__file__)),title = "Select an image to upload to model",filetypes = (("jpeg files","*.jpg *.jpeg"),))
+		
+		#use PIL to convert image to usable format
+		selection = ImageTk.PhotoImage(Image.open(img).resize((220, 220), Image.ANTIALIAS))
+		imgLabel.config(image=selection)
+		imgLabel.photo_ref=selection #keep reference to new image
+
+		imgIcon = Image.open(img).resize((32, 32), Image.ANTIALIAS) #icon image for passing into ML algorithm
+		imgIcon.save('userIcon.jpg','JPEG')
+
+		iconPA = cv2.imread('userIcon.jpg')
+		iconPA = cv2.cvtColor(iconPA, cv2.COLOR_BGR2RGB) #cv2.imread converts image to BGR, this converts it back to RGB
+		
+		iconPA = np.concatenate(iconPA)
+
+		tempArray = []
+		for i in range(3):
+			for j in range(len(iconPA)):
+				tempArray.append(iconPA[j][i])
+
+		iconPA = tempArray
+		userFlag = 1
+
+	upload = Image.open('placeholderImg.png').resize((220, 220), Image.ANTIALIAS) #scale image, keep ratio
+	render = ImageTk.PhotoImage(upload)
+
+	titleLabel = Label(root, text="CarbonCamera\n", font=LARGE_FONT)
+	imgLabel = Label(root, image=render)
+	uploadButton = Button(root, text="Upload image..", command=open)
+	spacerLabel = Label(root, text='')
+	runButton = Button(root, text="Run model", activeforeground="green", fg="green", font=BOLD_FONT, command=lambda: ccMain.startModel(userFlag, iconPA))
+
+	titleLabel.pack()
+	uploadButton.pack()
+	imgLabel.pack()
+	spacerLabel.pack()
+	runButton.pack()
+
+	atexit.register(exit_handler) #define functionality at program exit
+
+	root.mainloop()
---
+++ ccMain.py
@@ -0,0 +1,81 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+import time
+import ccDataHandler
+
+def startModel(flag, PA):
+	beginTime = time.time()
+
+	# Parameter definitions
+	batch_size = 100
+	learning_rate = 0.005
+	max_steps = 1000
+
+	# Prepare data
+	data_sets = ccDataHandler.load_data(flag, PA)
+
+	# -----------------------------------------------------------------------------
+	# Prepare the TensorFlow graph
+	# -----------------------------------------------------------------------------
+
+	# Define input placeholders
+	images_placeholder = tf.compat.v1.placeholder(tf.float32, shape=[None, 3072])
+	labels_placeholder = tf.compat.v1.placeholder(tf.int64, shape=[None])
+
+	# Define variables (these are the values we want to optimize)
+	weights = tf.Variable(tf.zeros([3072, 100]))
+	biases = tf.Variable(tf.zeros([100]))
+
+	# Define the classifier's result
+	logits = tf.matmul(images_placeholder, weights) + biases
+
+	# Define the loss function
+	loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,
+	  labels=labels_placeholder))
+
+	# Define the training operation
+	train_step = tf.compat.v1.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+
+	# Operation comparing prediction with true label
+	correct_prediction = tf.equal(tf.argmax(logits, 1), labels_placeholder)
+
+	# Operation calculating the accuracy of our predictions
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+	# -----------------------------------------------------------------------------
+	# Run the TensorFlow graph
+	# -----------------------------------------------------------------------------
+	
+	with tf.compat.v1.Session() as sess:
+		# Initialize variables
+		sess.run(tf.compat.v1.global_variables_initializer())
+		print('\n')
+		# Repeat max_steps times
+		for i in range(max_steps):
+			# Generate input data batch
+			indices = np.random.choice(data_sets['images_train'].shape[0], batch_size)
+			images_batch = data_sets['images_train'][indices]
+			labels_batch = data_sets['labels_train'][indices]
+			# Periodically print out the model's current accuracy
+			if i % 100 == 0:
+				train_accuracy = sess.run(accuracy, feed_dict={images_placeholder: images_batch, labels_placeholder: labels_batch})
+				print('Step {:5d}: training accuracy {:g}'.format(i, train_accuracy))
+
+			# Perform a single training step
+			sess.run(train_step, feed_dict={images_placeholder: images_batch, labels_placeholder: labels_batch})
+
+		# After finishing the training, evaluate on the test set
+		for i in range(len(data_sets['images_test'])): #for each test image, display the accuracy at which the model correctly predicts the images label
+			test_accuracy = sess.run(accuracy, feed_dict={images_placeholder: data_sets['images_test'][[i]], labels_placeholder: data_sets['labels_test'][[i]]})
+			if(test_accuracy>0): #if match accuracy > 0, display the class of identified object (this should only be true once per image)
+				print("-"*30)
+				print("Image classified as:", data_sets['classes'][i]) #display label
+				print("-"*30)
+
+	
+	endTime = time.time()
+	print('Total time: {:5.2f}s'.format(endTime - beginTime))
